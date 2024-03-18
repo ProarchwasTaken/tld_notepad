811621cb20ae639a009a34d25b468515a5eb30ea
Tyler <lorenarcdev@gmail.com>
2024-03-17 22:23:51 -0400

    A message will print to the console whenever the module is imported.

--- src/backend.cpp
+++ src/backend.cpp
@@ -1,6 +1,8 @@
 #include <pybind11/pybind11.h>
+#include <iostream>
 
 
 PYBIND11_MODULE(backend, m) {
+  std::cout << "Tyler's Notepad - v1.0";
   m.doc() = "The backend code for the notepad application.";
 }
